9571ad9802a26f741af8b0432bb013d9412119f2
Simone Demuro <simone.demuro@techedge.com>
2018-04-20 21:46:26 +0300

    Resize HQ

--- IMOMS Display Mockup Framework/IMOMS Display Mockup Framework/ImagePreview.cs
+++ IMOMS Display Mockup Framework/IMOMS Display Mockup Framework/ImagePreview.cs
@@ -15,30 +15,18 @@ namespace IMOMS_Display_Mockup_Framework
         public ImagePreview()
         {
             InitializeComponent();
-            List<string> list= new List<string>();
-            list.Add("haa");
-            list.Add("gaa");
-            list.Add("faa");
-            list.Add("eaa");
-            list.Add("daa");
-            list.Add("caa");
-            list.Add("baa");
-            DsvDisplay.createDisplay(list, "NomeDashboard");
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
-        {   
-            //Create an image list START 
-            Bitmap img_overlay = new Bitmap("Components/comp01.png");
-            img_overlay = DsvDisplay.resizeToComponentSize(img_overlay);
-            Queue<Bitmap> prova = new Queue<Bitmap>();
-            prova.Enqueue(img_overlay);
-            prova.Enqueue(img_overlay);
-            prova.Enqueue(img_overlay);
-
-            // Create a list END
-
-            Bitmap Result = DsvDisplay.setComponents(prova);
+        {
+            Queue<Bitmap> Comp = new Queue<Bitmap>();
+            Comp.Enqueue(new Bitmap("Components/comp01.png"));
+            Comp.Enqueue(new Bitmap("Components/comp01.png"));
+            Comp.Enqueue(new Bitmap("Components/comp01.png"));
+            Comp.Enqueue(new Bitmap("Components/comp01.png"));
+            Comp.Enqueue(new Bitmap("Components/comp01.png"));
+            Comp.Enqueue(new Bitmap("Components/comp01.png"));
+            Bitmap Result = DsvDisplay.setComponents(Comp);
             pictureBox1.Image = (Image)Result;
         }
     }
