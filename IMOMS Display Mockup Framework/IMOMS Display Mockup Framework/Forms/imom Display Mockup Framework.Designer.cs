7d1f17a38a7def89d58f066f93de9bc291b45837
Simone Demuro <simone.demuro@techedge.com>
2018-04-20 22:00:07 +0300

    added facilities

--- IMOMS Display Mockup Framework/IMOMS Display Mockup Framework/Forms/imom Display Mockup Framework.Designer.cs
+++ IMOMS Display Mockup Framework/IMOMS Display Mockup Framework/Forms/imom Display Mockup Framework.Designer.cs
@@ -148,12 +148,14 @@
             resources.ApplyResources(this.openDisplaysFolderButton, "openDisplaysFolderButton");
             this.openDisplaysFolderButton.Name = "openDisplaysFolderButton";
             this.openDisplaysFolderButton.UseVisualStyleBackColor = true;
+            this.openDisplaysFolderButton.Click += new System.EventHandler(this.openDisplaysFolderButton_Click);
             // 
             // openDashboardsFolderButton
             // 
             resources.ApplyResources(this.openDashboardsFolderButton, "openDashboardsFolderButton");
             this.openDashboardsFolderButton.Name = "openDashboardsFolderButton";
             this.openDashboardsFolderButton.UseVisualStyleBackColor = true;
+            this.openDashboardsFolderButton.Click += new System.EventHandler(this.openDashboardsFolderButton_Click);
             // 
             // groupBox2
             // 
@@ -170,21 +172,23 @@
             resources.ApplyResources(this.button2, "button2");
             this.button2.Name = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
             resources.ApplyResources(this.button1, "button1");
             this.button1.Name = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
-            // Form1
+            // imomsDisplayMockupFramework
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.displaysGroupBox);
-            this.Name = "Form1";
+            this.Name = "imomsDisplayMockupFramework";
             this.displaysGroupBox.ResumeLayout(false);
             this.displaysGroupBox.PerformLayout();
             this.groupBox1.ResumeLayout(false);
